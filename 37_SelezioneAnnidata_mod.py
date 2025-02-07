6cab94094de72170b046047ade6edf2eea85fe99
LotitoAndrea <lotito.andrea@tiisgrassi.edu.it>
2025-02-07 11:06:03 +0100

    da finire dopo

---
+++ 37_SelezioneAnnidata_mod.py
@@ -0,0 +1,16 @@
+print("Benvenuto nel mio programma di conversazione")
+print()
+risposta=input("Ti piace andare in bicicletta? Rispondi sì o no: ")
+if risposta=="sì":
+    print("Molto bene! Ti terrai in forma.")
+    risposta2 = input("Ti piace anche il basket? ")
+    if risposta2=="sì":
+        print("Allora sei un atleta!")
+    else:
+        print("Uno sport è meglio di niente!")
+elif risposta == "no":
+    risposta3 = input("Ti piace qualche altro sport? Rispondi sì o no: ")
+    if risposta3=="sì":
+        print("Bene! Lo sport è importante per la salute.")
+    else:
+        print("Non tutti sono sportivi.")
